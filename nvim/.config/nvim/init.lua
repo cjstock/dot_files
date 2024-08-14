59393dfdf6fa7eb03cc8944393653a8b86a871de
Corey Stock <coreystock@gmail.com>
2024-08-14 12:59:57 -0700

    removed zellij theme; fixed depricated which-key warnings

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -281,12 +281,12 @@ require("lazy").setup({
 			require("which-key").setup()
 
 			-- Document existing key chains
-			require("which-key").register({
-				["<leader>c"] = { name = "[C]ode", _ = "which_key_ignore" },
-				["<leader>d"] = { name = "[D]ocument", _ = "which_key_ignore" },
-				["<leader>r"] = { name = "[R]ename", _ = "which_key_ignore" },
-				["<leader>s"] = { name = "[S]earch", _ = "which_key_ignore" },
-				["<leader>w"] = { name = "[W]orkspace", _ = "which_key_ignore" },
+			require("which-key").add({
+				{ "<leader>c", group = "[C]ode" },
+				{ "<leader>d", group = "[D]ocument" },
+				{ "<leader>r", group = "[R]ename" },
+				{ "<leader>s", group = "[S]earch" },
+				{ "<leader>w", group = "[W]orkspace" },
 			})
 		end,
 	},
@@ -552,9 +552,21 @@ require("lazy").setup({
 						["rust_analyzer"] = {},
 					},
 				},
-				html = {},
-				htmx = {},
-				prettier = {},
+				html = {
+					settings = {
+						filetypes = { "rs", "html" },
+					},
+				},
+				htmx = {
+					settings = {
+						filetypes = { "rs", "html" },
+					},
+				},
+				prettier = {
+					settings = {
+						filetypes = { "rs", "html" },
+					},
+				},
 				-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
 				--
 				-- Some languages (like typescript) have entire language plugins that can be useful:
@@ -832,7 +844,7 @@ require("lazy").setup({
 		"nvim-treesitter/nvim-treesitter",
 		build = ":TSUpdate",
 		opts = {
-			ensure_installed = { "bash", "c", "html", "lua", "markdown", "vim", "vimdoc", "rust" },
+			ensure_installed = { "bash", "html", "lua", "markdown", "vim", "vimdoc", "rust" },
 			-- Autoinstall languages that are not installed
 			auto_install = true,
 			highlight = {
@@ -840,9 +852,9 @@ require("lazy").setup({
 				-- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
 				--  If you are experiencing weird indenting issues, add the language to
 				--  the list of additional_vim_regex_highlighting and disabled languages for indent.
-				additional_vim_regex_highlighting = { "ruby" },
+				additional_vim_regex_highlighting = { "ruby", "html" },
 			},
-			indent = { enable = true, disable = { "ruby" } },
+			indent = { enable = true, disable = { "ruby", "html" } },
 		},
 		config = function(_, opts)
 			-- [[ Configure Treesitter ]] See `:help nvim-treesitter`
